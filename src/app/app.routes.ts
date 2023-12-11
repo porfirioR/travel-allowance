aa3f728a4ab23f27f9d367ec77bc995d27eb0cc0
PorfirioR <porfirioperezgon@gmail.com>
2023-12-11 01:10:42 -0300

    first-step

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'configuration',
+    title: 'Configuration',
+    component: ConfigurationComponent
+  }
+];
