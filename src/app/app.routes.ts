94fbe9f6aa46903d88de94b48f04876e2a6080d0
PorfirioR <porfirioperezgon@gmail.com>
2023-12-11 01:22:28 -0300

    first-step

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    title: 'Travel Allowance',
+    component: PrincipalComponent
+  },
   {
     path: 'configuration',
     title: 'Configuration',
